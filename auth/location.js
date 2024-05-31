a1c417d18de925187fd667711ec30d3db03ebd61
christian birego <93344052+birego@users.noreply.github.com>
2024-06-01 00:35:43 +0200

    location

--- auth/location.js
+++ auth/location.js
@@ -1,17 +1,16 @@
 import jwt from 'jsonwebtoken';
-const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) {
-      return res.status(401).json({ message: 'No token provided' });
-    }
-    jwt.verify(token, secretKey, (err, decoded) => {
-      if (err) {
-        return res.status(401).json({ message: 'Invalid token' });
-      }
-      req.user = decoded; // Ajouter les informations utilisateur à la requête
-      next();
-    });
-  };
-  
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, secretKey, (err, user) => {
+    if (err) return res.sendStatus(403);
+    console.log('Token decoded, user:', user);
+    req.user = user;
+    next();
+  });
+}
+
 
   export default authenticateToken;
