47aad83d34eba4765792685294103e1c820f23f1
christian birego <93344052+birego@users.noreply.github.com>
2024-06-01 00:42:29 +0200

    key

--- auth/location.js
+++ auth/location.js
@@ -1,4 +1,11 @@
+// auth/location.js
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Charger les variables d'environnement
+
+const secretKey = process.env.SECRET_KEY || 'RAT_PI';
+
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
@@ -12,5 +19,4 @@ function authenticateToken(req, res, next) {
   });
 }
 
-
-  export default authenticateToken;
+export default authenticateToken;
